0d5a63e3d00643083055813be5a792bbda68e7ab
Nithish <nithishm612345@gmail.com>
2025-11-30 12:44:50 +0530

    cors update

--- backend/server.js
+++ backend/server.js
@@ -14,16 +14,35 @@ const orderRoutes = require("./routes/orderRoutes");
 const app = express();
 connectDB();
 
-const corsOptions = {
-  origin: process.env.CLIENT_URL || "http://localhost:5173",
-  credentials: true,
-};
+// const corsOptions = {
+//   origin: process.env.CLIENT_URL || "http://localhost:5173",
+//   credentials: true,
+// };
+
+const allowedOrigins = [
+  "http://localhost:5173", // local dev
+  process.env.CLIENT_URL, // production Vercel frontend
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("CORS not allowed"));
+      }
+    },
+    credentials: true,
+  })
+);
 
 // middlewares
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors(corsOptions));
 
 // routes
 app.use("/api/auth", authRoutes);
